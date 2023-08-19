8bdecf873a6e4c364929edce09d4661291d8fa3b
pratheek ms <2942624+prthkms@users.noreply.github.com>
2023-08-19 14:52:06 +0530

    FEAT: rename

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     requirements = [line.strip() for line in f if line]
 
 setup(
-    name="dev-gpt",
+    name="tdd-gpt",
     version="0.1",
-    description="Autonomous agent for web app development using GPT-4",
+    description="Autonomous agent for test driven development using GPT-4",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gimlet-ai/dev-gpt",
+    url="https://github.com/gimlet-ai/tdd-gpt-agent",
     author="Rajiv Poddar",
     author_email="rajiv@gimlet.ai",
     license="MIT",
-    keywords="dev-gpt autonomous coding agent web app development gpt-4 langchain codegen",
+    keywords="tdd-gpt dev-gpt autonomous coding agent web app development gpt-4 langchain codegen",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     python_requires=">=3.9",
     install_requires=requirements,
@@ -28,7 +28,7 @@ setup(
     },
     entry_points={
         "console_scripts": [
-            "dev-gpt=dev_gpt.main:main",
+            "tdd-gpt=tdd_gpt.main:main",
         ],
     },
 )
